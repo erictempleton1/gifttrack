cec72ad6f5838c16f0e1dbf8ab1855dc6471a1cf
ETempleton <etempleton@r2integrated.com>
2017-04-14 15:32:57 -0400

    working on adding forms

--- gifttrack/views.py
+++ gifttrack/views.py
@@ -3,9 +3,14 @@ from __future__ import unicode_literals
 
 from .models import Gift
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
     all_gifts = Gift.objects.all()
     context = {'all_gifts': all_gifts}
     return render(request, 'gifttrack/index.html', context)
+
+
+def create(request):
+    return render(request, 'gifttrack/create.html')
