1e522855024eeea930cbd2f0bba760d49ee097b2
erictempleton1 <erictempleton1@gmail.com>
2017-05-02 07:21:36 -0400

    working on models

--- gifttrack/models.py
+++ gifttrack/models.py
@@ -9,13 +9,35 @@ from datetime import datetime
 
 
 @python_2_unicode_compatible
+class GiftList(models.Model):
+    name = models.CharFields(max_length=500)
+    save_date = models.DateTimeField(default=datetime.now)
+    edit_date = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class GiftListFields(models.Model):
+    name = models.CharFields(max_length=150)
+    save_date = models.DateTimeField(default=datetime.now)
+    edit_date = models.DateTimeField(default=datetime.now)
+    gift_list = models.ForeignKey(GiftList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
 class Gift(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
     gift_desc = models.CharField(max_length=500)
     gift_from = models.CharField(max_length=200)
     gift_notes = models.CharField(max_length=500)
     save_date = models.DateTimeField(default=datetime.now)
     edit_date = models.DateTimeField(default=datetime.now)
+    gift_list = models.ForeignKey(GiftList, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.gift_desc
